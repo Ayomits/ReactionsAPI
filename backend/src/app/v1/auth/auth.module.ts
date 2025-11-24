6088b9b2e33ab86a68915c20ee7b64df20299f42
a2yomi <a2yomidiscord@gmail.com>
2025-11-24 23:44:09 +0300

    fix: auth module

--- backend/src/app/v1/auth/auth.module.ts
+++ backend/src/app/v1/auth/auth.module.ts
@@ -1,12 +1,9 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
-import { AppConfig } from 'src/config';
 import { AuthController } from './auth.controller';
 import { TokensModule } from '../tokens/tokens.module';
 import { AuthGuard } from 'src/guards/auth.guard';
-
-AppConfig.init();
 
 @Module({
   imports: [forwardRef(() => UserModule), forwardRef(() => TokensModule)],
